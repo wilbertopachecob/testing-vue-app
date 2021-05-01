0d4498b5dd5dc1e6348d5329c60a33f24038d527
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2021-04-30 22:42:36 -0500

    done with homework for chapter 3

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-      Hello, World!
+    Hello, World!
+    <Child :testProp="123" />
+    <a href="https://google.com">Link</a>
+    <p style="color: red">Paragraph</p>
   </div>
 </template>
 
 <script>
-export default {}
+import Child from './Child'
+export default {
+  components: {
+    Child
+  }
+}
 </script>
 
-<style>
-</style>
+<style></style>
--- test/unit/TestComponent.spec.js
+++ test/unit/TestComponent.spec.js
@@ -1,6 +1,33 @@
 import { shallowMount } from '@vue/test-utils'
 import TestComponent from '../../src/components/TestComponent'
-test('renders Hello, World!', () => {
-  // Add test code here
-  expect(shallowMount(TestComponent).text()).toBe('Hello, World!')
+import Child from '../../src/components/Child'
+
+describe('TestComponent.vue', () => {
+  test('renders Hello, World!', () => {
+    // Add test code here
+    expect(shallowMount(TestComponent).text()).toContain('Hello, World!')
+  })
+  test('renders ChildC', () => {
+    // Add test code here
+    expect(shallowMount(TestComponent).findComponent(Child).exists()).toBe(true)
+  })
+  test('renders ChildC with prop testProp', () => {
+    // Add test code here
+    const testProp = 123
+    expect(
+      shallowMount(TestComponent).findComponent(Child).props().testProp
+    ).toBe(testProp)
+  })
+  test('check link href value', () => {
+    // Add test code here
+    expect(shallowMount(TestComponent).find('a').element.href).toBe(
+      'https://google.com/'
+    )
+  })
+  test('p color = red', () => {
+    // Add test code here
+    expect(shallowMount(TestComponent).find('p').element.style.color).toBe(
+      'red'
+    )
+  })
 })
